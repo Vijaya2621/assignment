61403459e4011eafebe24f0ab97e18a349ee63bd
vijaya <vijayakumariwins@gmail.com>
2024-05-08 12:14:46 +0530

    env

--- apps/utils/env.config.ts
+++ apps/utils/env.config.ts
@@ -1,14 +1,14 @@
-import { config } from 'dotenv';
-import * as path from 'path';
-// config();
+// import { config } from 'dotenv';
+// import * as path from 'path';
+// // config();
 
-config({ path: path.join(__dirname, '.env') });
-export const PORT = process.env.DB_PORT;
-export const DB_HOST = process.env.DB_HOST || 'localhost';
-export const DB_TYPE = process.env.DB_TYPE || 'mysql';
-export const POSTGRES_PORT = process.env.POSTGRES_PORT || 3306;
-export const DB_USERNAME = process.env.DB_USERNAME || 'dits';
-export const DB_PASSWORD = process.env.password || 'password';
-export const DATABASE = process.env.DATABASE || 'hospital_management';
-export const SECRET_KEY = process.env.SECRET_KEY || 'JWT_SECRET_KEY';
-export const EXPIRESIN = process.env.EXPIRESIN || '1h';
+// config({ path: path.join(__dirname, '.env') });
+// export const PORT = process.env.DB_PORT;
+// export const DB_HOST = process.env.DB_HOST || 'localhost';
+// export const DB_TYPE = process.env.DB_TYPE || 'mysql';
+// export const POSTGRES_PORT = process.env.POSTGRES_PORT || 3306;
+// export const DB_USERNAME = process.env.DB_USERNAME || 'dits';
+// export const DB_PASSWORD = process.env.password || 'password';
+// export const DATABASE = process.env.DATABASE || 'hospital_management';
+// export const SECRET_KEY = process.env.SECRET_KEY || 'JWT_SECRET_KEY';
+// export const EXPIRESIN = process.env.EXPIRESIN || '1h';
